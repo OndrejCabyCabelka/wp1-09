f114c222415193fae3b1cf3332180a1936d8ba99
OndrejCabyCabelka <79140324+OndrejCabyCabelka@users.noreply.github.com>
2021-06-08 14:39:12 +0200

    index.php

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
+
+
 <?php
 
 $submit = filter_input (INPUT_POST, 'submit');
 
 $model = filter_input (INPUT_POST,"model");
-
+$cars = $_POST['cars'];
 
 ?>
-<?php
-if(isset($submit)) {
-  echo "Formulář byl odeslán <br>";
 
-} else  ?>
-<form action= "index.php" method="post">
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,34 +16,43 @@ if(isset($submit)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>volkswagen.cz</title>
+    <title>Volkswagen.cz </title>
 </head>
 <body>
-
+<?php
+if(isset($submit)) {
+  echo "Formulář byl odeslán <br>";
+  foreach ($cars as $car) {
+    echo $car. "<br>";
+  }
+  } else  ?>
+<form action= "index.php" method="post">
 <h1>Volkswagen</h1>
+
 
 <label for="cars">Vyber model:</label>
 <select name="cars" id="cars">
-  <option value="felicie">Polo</option>
-  <option value="octavia">Golf</option>
-  <option value="fabie">Passat</option>
-  <option value="kodiaq">Tiguan</option>
-  <option value="superb">Touran</option>
-  <option value="scala">up!;</option>
+  <option value="Polo">Polo</option>
+  <option value="Golf">Golf</option>
+  <option value="Passat">Passat</option>
+  <option value="Tiguan">Tiguan</option>
+  <option value="Touran">Touran</option>
+  <option value="up">up!</option>
+
 <br>  
-<input type="radio" id="benzin" name="car" value="benzin">
+<input type="radio" id="benzin" name="cars[]" value="benzin">
 <label for="benzin">Benzín </label>
 <br>
-<input type="radio" id="diesel" name="car" value="diesel">
+<input type="radio" id="diesel" name="cars[]" value="diesel">
 <label for="diesel">Diesel</label>
 <br>
-<input type="radio" id="LPG" name="car" value="LPG">
+<input type="radio" id="LPG" name="cars[]" value="LPG">
 <label for="LPG">LPG</label>
 <br>
-<input type="radio" id="elektro" name="car" value="elektro">
+<input type="radio" id="elektro" name="cars[]" value="elektro">
 <label for="elektro">Elektro</label>
 <br>
-<input type="radio" id="hybrid" name="car" value="hybrid">
+<input type="radio" id="hybrid" name="cars[]" value="hybrid">
 <label for="hybrid">Hybrid</label>
 <br>
 <input type="checkbox" id="car1" name="cars[]" value="sedacky">
@@ -71,7 +77,7 @@ if(isset($submit)) {
 <label for="motor"> Motor</label>
 <br>
 <label for="barvy">Barva:</label>
-<select name="barvy" id="barvy">
+<select name="cars[]" id="barvy">
   <option value="fialova">Fialová</option>
   <option value="zelena">Zelená</option>
   <option value="cervena">Červená</option>
